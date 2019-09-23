68c62ef8745aab093768ed7e0b912d622ccb5674
Ayushman Bilas Thakur <ayushmanbilasthakur@gmail.com>
2019-09-24 01:28:00 +0530

    page update

--- next.config.js
+++ next.config.js
@@ -7,3 +7,7 @@ module.exports = {
     return cfg;
   }
 };
+
+module.exports = withMDX({
+  extension: /\.md$/
+});
--- posts/first-blog.md
+++
@@ -1,5 +0,0 @@
----
-title: First Blog
-date: 2019-09-23T19:49:08.811Z
----
-# Hello World
