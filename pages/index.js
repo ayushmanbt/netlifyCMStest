b14cb0f7fa803d857100e0d6adb12d32089185f1
Ayushman Bilas Thakur <ayushmanbilasthakur@gmail.com>
2019-09-24 00:29:00 +0530

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import content from "../content/home.md";
+
+function index() {
+  let {
+    html,
+    attributes: { title, cats }
+  } = content;
+  return (
+    <article>
+      <h1>{title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: html }} />
+      <ul>
+        {cats.map((cat, k) => (
+          <li key={k}>
+            <h2>{cat.name}</h2>
+            <p>{cat.description}</p>
+          </li>
+        ))}
+      </ul>
+    </article>
+  );
+}
+
+export default index;
